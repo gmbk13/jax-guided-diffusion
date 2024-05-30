d8bbc83aaeda302687c3900ede3b2710ec2a8eb2
gmbk13 <gumbreaker13@gmail.com>
2024-05-30 19:50:31 +0900

    Update core.py

--- jaxtorch/core.py
+++ jaxtorch/core.py
@@ -41,7 +41,6 @@ class PRNG(object):
 class ContextRandom(object):
     """Lives inside a Context and provides convenience functions for
 random number generation that use the Context's stateful PRNG.
-
     """
     def __init__(self, rng):
         self.rng = rng
@@ -98,7 +97,7 @@ class Context(object):
         elif isinstance(par, str):
             return self.px[par]
         else:
-            raise TypeError('Expected a Param for indexing into Context')
+            raise TypeError('Expected a Param or str for indexing into Context')
 
     def __setitem__(self, par, value):
         if isinstance(par, Param):
@@ -106,7 +105,7 @@ class Context(object):
         elif isinstance(par, str):
             self.px[par] = value
         else:
-            raise TypeError('Expected a Param for indexing into Context')
+            raise TypeError('Expected a Param or str for indexing into Context')
 
     def tree_flatten(self):
         return (self.px, self.rng.split()), (self.mode,)
@@ -116,7 +115,6 @@ class Context(object):
         (px, key) = values
         (mode,) = aux
         return Context(px, key, mode=mode)
-
 
 class Module(object):
     def __call__(self, cx: Context, *args, **kwargs):
@@ -129,7 +127,6 @@ class Module(object):
     def self_named_modules(self):
         """Yields a sequence of (str, Module) for direct children of this
            module. May be overridden.
-
         """
         for (name, val) in self.__dict__.items():
             if isinstance(val, Module):
@@ -138,17 +135,15 @@ class Module(object):
     def self_named_parameters(self):
         """Yields a sequence of (str, Param) for direct children of this
            module. May be overridden.
-
         """
         for (name, val) in self.__dict__.items():
             if isinstance(val, Param):
                 yield (name, val)
 
     def self_init_weights(self, cx):
-        """Initializes weights for this network's parameters. May be overriden
+        """Initializes weights for this network's parameters. May be overridden
            for custom initialization. Child modules are initialized
            before parents.
-
         """
         for (name, par) in self.self_named_parameters():
             if par.initializer is not None:
@@ -157,7 +152,6 @@ class Module(object):
     def init_weights(self, key):
         """Attaches names to parameters and returns initialized dict of
            parameters by name.
-
         """
         self.labeled_parameters_()
         cx = Context({}, key)
@@ -225,7 +219,7 @@ class Module(object):
                     print(msg, file=sys.stderr)
                     continue
 
-            px[p.name] = jax.numpy.asarray(state[k])
+            px[p.name] = jnp.asarray(state[k])
         return px
 
     def _get_name(self):
@@ -233,7 +227,6 @@ class Module(object):
 
     def extra_repr(self) -> str:
         r"""Set the extra representation of the module
-
         To print customized extra information, you should re-implement
         this method in your own modules. Both single-line and multi-line
         strings are acceptable.
